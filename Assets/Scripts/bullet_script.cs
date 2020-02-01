1ff0375957e0e600bb41047d22a92287b327993d
SpencerWF <spencer.walkerfooks@gmail.com>
2020-02-01 22:31:46 +0000

    Adding firing.

---
+++ Assets/Scripts/bullet_script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet_script : MonoBehaviour
+{
+
+    public float speed = 20;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
