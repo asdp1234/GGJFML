0892610d07b51222c00b7720e6a9ebcc55ac0905
Amirali Mohayaee <amig90@yahoo.com>
2020-02-01 15:46:01 +0000

    Created Prototype Navmesh

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This is used by the AI (while attacheck to the 
+// Player), which makes it easier for the AI to find the 
+// GO needed
+// So, attack this to the player, please
+public class PlayerManager : MonoBehaviour
+{
+    #region Singleton
+
+    public static PlayerManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    #endregion
+
+}
