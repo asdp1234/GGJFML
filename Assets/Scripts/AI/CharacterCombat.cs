f097008e5947b961cf78b38f5ab0116b99634ffb
Amirali Mohayaee <amig90@yahoo.com>
2020-02-01 18:40:11 +0000

    Added new Music and Navmesh behaviour
    New navmesh behaviour for enemies allows for travel between nodes. Not fully complete yet.

---
+++ Assets/Scripts/AI/CharacterCombat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCombat : MonoBehaviour
+{
+    [SerializeField]
+    GameObject playerReference;
+
+    [SerializeField]
+    GameObject EnemyReference;
+
+
+    public void Attacking(float PlayerHealth)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/AI/EnemyMoveGoal.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMoveGoal : MonoBehaviour
+{
+    [SerializeField]
+    public Transform[] m_goals; // container of nodes
+
+    // Current destination set for each
+    // node-to-node travel
+    private int destPoint = 0;
+
+    [SerializeField]
+    private NavMeshAgent agent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        // Set false for continous movement between nodes 
+        // as it's set to on by default
+        agent.autoBraking = false;
+
+        GoNextPoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void GoNextPoint()
+    {
+        // prerequisite check for container being empty
+        if (m_goals.Length == 0)
+        {
+            print("Node container empty. Possible incomplete NavMesh.");
+            return;
+        }
+
+        // Move to current selected node in container
+        agent.destination = m_goals[destPoint].position;
+
+        // When we move to the new point, the container will
+        // increment by one node so we get the new updated position
+        destPoint = (destPoint + 1) % m_goals.Length;
+    }
+}
