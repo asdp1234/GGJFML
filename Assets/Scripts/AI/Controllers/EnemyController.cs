a750e70bfce1b1eaaf9f26797413642867f7871f
Amirali Mohayaee <amig90@yahoo.com>
2020-02-01 18:40:11 +0000

    Added new Music and Navmesh behaviour
    New navmesh behaviour for enemies allows for travel between nodes. Not fully complete yet.

--- Assets/Scripts/AI/Controllers/EnemyController.cs
+++ Assets/Scripts/AI/Controllers/EnemyController.cs
@@ -11,15 +11,15 @@ public class EnemyController : MonoBehaviour
     [SerializeField]
     private float distToPlayer;
 
+
     // AI-specific
     Transform m_target;
     NavMeshAgent m_agent;
+    GameObject m_player;
 
-
-    // Start is called before the first frame update
     void Start()
     {
-        GameObject m_player = GameObject.Find("Player");
+        m_player = GameObject.Find("Player");
         // AI searches for the player
         m_target = m_player.transform;
 
@@ -35,7 +35,29 @@ public class EnemyController : MonoBehaviour
         if (distToPlayer <= m_lookRadius)
         {
             m_agent.SetDestination(m_target.position);
+
+            if (distToPlayer <= m_agent.stoppingDistance)
+            {
+                // Place Attacking lines here
+                FaceTarget();
+            }
         }
+    }
+
+    void FaceTarget()
+    {
+        // Getting direction to player with forward vector
+        Vector3 direction = (m_target.position - transform.position).normalized;
+        // Gets correct rotation matrix for facing when following player
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 4.0f);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        collision.Equals(m_player);
+
+        // Do damage to player
     }
 
     // Showing draw distance of the enemy
