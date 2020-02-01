784cd96a124c423989a6adc3f5244e6c622e1c7b
SpencerWF <spencer.walkerfooks@gmail.com>
2020-02-01 12:58:16 +0000

    Pickup Functionality

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -7,6 +7,7 @@ public class movement : MonoBehaviour
     public float move_speed = 5f;
     public float jump_height = 5f;
     public Rigidbody rb;
+    public bool item_state = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,7 +25,15 @@ public class movement : MonoBehaviour
 
     void Jump(){
         if (Input.GetButtonDown("Jump")){
-	    rb.AddForce(new Vector3(0f,jump_height,0f), ForceMode.Impulse);
-	}
+	        rb.AddForce(new Vector3(0f,jump_height,0f), ForceMode.Impulse);
+	    }
+    }
+
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.name == "item") {
+            item_state = true;
+            Debug.Log("Collision Detected");
+            GameObject.Destroy(col.gameObject,1);
+        }
     }
 }
