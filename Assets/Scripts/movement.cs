a9963bda6b5349326d65056dafd06014db39ed9d
SpencerWF <spencer.walkerfooks@gmail.com>
2020-02-01 11:41:40 +0000

    Moving and Jumping functionality

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float move_speed = 5f;
+    public float jump_height = 5f;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	Jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+	transform.position += movement * Time.deltaTime * move_speed;
+
+    }
+
+    void Jump(){
+        if (Input.GetButtonDown("Jump")){
+	    rb.AddForce(new Vector3(0f,jump_height,0f), ForceMode.Impulse);
+	}
+    }
+}
