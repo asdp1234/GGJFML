aba75c05199af934d3d49bf2c536692204008ca3
SpencerWF <spencer.walkerfooks@gmail.com>
2020-02-02 12:10:54 +0000

    Bullet and Gun functionality

--- Assets/Scripts/bullet_script.cs
+++ Assets/Scripts/bullet_script.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class bullet_script : MonoBehaviour
 {
 
-    public float speed = 20;
+    public float speed = 200;
     public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
@@ -17,5 +17,12 @@ public class bullet_script : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter() {
+        GameObject.Destroy(this.gameObject,0);
+    }
+    void OnCollisionEnter(Collision col) {
+        GameObject.Destroy(this.gameObject,0);
     }
 }
